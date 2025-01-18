b516b6e6f44dc5e6dc403be6f4645a86117d314a
Arodud <aroslavdudkov194@gmail.com>
2025-01-18 22:23:48 +0000

    dotnet initiation

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using NBitcoin;
+
+namespace _125350929
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World! " + new Key().GetWif(Network.Main));
+    }
+  }
+}
