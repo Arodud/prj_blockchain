47ba681026c20f4820d635c882eb480da7d6823a
Arodud <aroslavdudkov194@gmail.com>
2025-01-18 22:47:59 +0000

    library test

--- Program.cs
+++ Program.cs
@@ -8,7 +8,30 @@ namespace _125350929
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World! " + new Key().GetWif(Network.Main));
+      Key privateKey = new Key();
+      PubKey publicKey = privateKey.PubKey;
+      Console.WriteLine(publicKey.GetAddress(ScriptPubKeyType.Legacy, Network.Main));
+      Console.WriteLine(publicKey.GetAddress(ScriptPubKeyType.Legacy, Network.TestNet));
+
+      var publicKeyHash = new KeyId("14836dbe7f38c5ac3d49e8d790af808a4ee9edcf");
+
+      var testNetAddress = publicKeyHash.GetAddress(Network.TestNet);
+      var mainNetAddress = publicKeyHash.GetAddress(Network.Main);
+    
+      Console.WriteLine(mainNetAddress.ScriptPubKey); // OP_DUP OP_HASH160 14836dbe7f38c5ac3d49e8d790af808a4ee9edcf OP_EQUALVERIFY OP_CHECKSIG
+      Console.WriteLine(testNetAddress.ScriptPubKey);
+
+      var paymentScript = publicKeyHash.ScriptPubKey;
+
+      var sameMainNetAddress = paymentScript.GetDestinationAddress(Network.Main);
+
+      Console.WriteLine(mainNetAddress == sameMainNetAddress); // True
+
+      var samePublicKeyHash = (KeyId) paymentScript.GetDestination();
+
+      var sameMainNetAddress2 = new BitcoinPubKeyAddress(samePublicKeyHash, Network.Main);
+
+      Console.WriteLine(mainNetAddress == sameMainNetAddress2); // True
     }
   }
 }
--- obj/Debug/net8.0/prj_blockchain.AssemblyInfo.cs
+++ obj/Debug/net8.0/prj_blockchain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prj_blockchain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+257ce64e58adb74258788ec664c5cd3e6c560c02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25bfe2f89404a5433b1f4a048b3377e892231ec1")]
 [assembly: System.Reflection.AssemblyProductAttribute("prj_blockchain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prj_blockchain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
